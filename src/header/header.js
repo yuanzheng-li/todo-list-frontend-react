6f189a0fecd3fdfb2968f8b1e61d840964127e8b
Yuanzheng Li <yuanzhengliwork@gmail.com>
2021-02-11 11:10:29 -0800

    Add Header component

---
+++ src/header/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Marvelous v2.0</h1>
+      <a href='#'>Delete all tasks</a>
+    </header>
+  );
+}
